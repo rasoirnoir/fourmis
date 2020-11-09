e7f1bcf25d069a6df59f68e40540632b80ee8d7b
William NORIS <w.noris@protonmail.com>
2020-11-09 22:22:04 +0100

    basic argparse

--- fourmis.py
+++ fourmis.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pygame, argparse, sys
+
 '''
 
     Implémentation en python de la fourmis de Langton
@@ -10,9 +12,36 @@
 
 '''
 
+#Définition des constantes
+FILE = ""
+FPS = 30
+VERSION = "0.1"
+DEBUG = False
+
+def version():
+    print("fourmis.py version {} by CactusPin".format(VERSION))
+
 def main():
     print('Lancement de la simu.')
+    version()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''La fourmis de Langton. 
+        Automate cellulaire célèbre implémenté en python
+    ''')
+
+    parser.add_argument("file", help="The file containing the game board as a matrix.")
+    parser.add_argument("-v", "--version", help="prints the version and exits.", action="store_true")
+    parser.add_argument("-s", "--speed", help="The speed of the game in frames per second (fps)")
+
+    args = parser.parse_args()
+
+    if args.version:
+        version()
+        sys.exit(0)
+    if args.speed:
+        FPS = int(args.speed)
+
+    FILE = args.file
     main()
 
