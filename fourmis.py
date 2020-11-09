b45afb164918d0e86d5262d038c9009b122ab2bd
William NORIS <w.noris@protonmail.com>
2020-11-09 21:59:38 +0100

    Création de la base (fourmis.py)

---
+++ fourmis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''
+
+    Implémentation en python de la fourmis de Langton
+    Règles du jeu :
+     - Si la fourmis se tient sur une case noire, elle tourne à gauche
+     - Si elle se trouve sur une case blanche, elle tourne à droite
+     - La case sur laquelle elle se tenait change de couleur
+
+'''
+
+def main():
+    print('Lancement de la simu.')
+
+if __name__ == '__main__':
+    main()
+
