f8982d86362f15a177528bbe7a8caa1c5dc1115d
William NORIS <w.noris@protonmail.com>
2021-06-14 21:48:29 +0200

    externalisation du board.

--- fourmis.py
+++ fourmis.py
@@ -1,26 +1,5 @@
 import time
-from kivy.app import App
-from kivy.uix.widget import Widget
-# from kivy.graphics import *
-import kivy.graphics as G
-
-
-class Board(Widget):
-    def __init__(self, **kwargs):
-        super(Board, self).__init__(**kwargs)
-        self.bind(pos=self.update_canvas)
-        self.bind(size=self.update_canvas)
-        self.update_canvas()
-
-    def update_canvas(self, *args):
-        self.canvas.clear()
-        with self.canvas:
-            G.Rectangle(pos=(10, 10), size=(500, 500))
-
-
-class FourmisApp(App):
-    def build(self):
-        return Board()
+from board import FourmisApp
 
 
 def fourmisModule():
